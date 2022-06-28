39ec5ba362bac7bb15906b11bf39b0d4fe8f65ba
Jekarinio <107714479+Jekarinio@users.noreply.github.com>
2022-06-28 12:20:02 +0300

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,26 @@
+string1 = ('Hello Python')
+print(string1[2])
+
+string2 = ('Hello Python')
+print(string2[-2])
+
+string3 = ('Hello Python')
+print((string3[:6]))
+
+string4 = ('Hello Python')
+print(string4[:-2])
+
+string5 = ('Hello Python')
+print(string5[1 :: 2])
+
+string6 = ('Hello Python')
+print(string6[:: 2])
+
+string7 = ('Hello Python')
+print(string7[:: -1])
+
+string8 = ('Hello Python')
+print(string8[-2: 1:-1])
+
+string9 = ('Hello Python')
+print(len(string9))
